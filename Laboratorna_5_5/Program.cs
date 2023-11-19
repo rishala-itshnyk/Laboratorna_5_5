98f0d67eef846f374d47535b8926411134d4c701
Rishala Itshnyk <rishala.itshnyk@gmail.com>
2023-11-19 22:30:33 +0200

    initial commit

---
+++ Laboratorna_5_5/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+
+namespace Program
+{
+    public class Program
+    {
+        public static string TenBase(int n, int resSys)
+        {
+            char[] symbols = {'0','1','2','3','4','5','6','7','8','9',
+                'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P',
+                'Q','R','S','T','U','V','W','X','Y','Z'};
+
+            if (n < resSys)
+            {
+                return symbols[n].ToString();
+            }
+
+            return TenBase(n / resSys, resSys) + symbols[n % resSys];
+        }
+
+        public static int F(int n, int resSys, int level, ref int depth)
+        {
+            if (level > depth)
+            {
+                depth = level;
+                Console.WriteLine("Рівень = " + level);
+            }
+
+            if (n > resSys)
+            {
+                return F(n / resSys, resSys,level + 1, ref depth);
+            }
+
+            return level;
+        }
+
+        static void Main()
+        {
+            int depth = 0;
+
+            Console.Write("Число: ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.Write("Система: ");
+            int resSys = int.Parse(Console.ReadLine());
+
+            string result = TenBase(number, resSys);
+            Console.WriteLine("Результат: " + result);
+
+            F(number, resSys, 1, ref depth);
+            Console.WriteLine("Глибина = " + (depth + 1));
+        }
+    }
+}
---
+++ Program.Tests/Tests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace Program.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        int resSys = 2;
+        int number = 20;
+
+        [Test]
+        public void TestTenBase()
+        {
+            string actual = Program.TenBase(number, resSys);
+
+            Assert.AreEqual("10100", actual);
+        }
+
+        [Test]
+        public void TestF()
+        {
+            int depth = 0;
+
+            int result = Program.F(number, resSys,1, ref depth);
+
+            Assert.AreEqual(4, result);
+            Assert.AreEqual(5, depth + 1);
+        }
+    }
+}
